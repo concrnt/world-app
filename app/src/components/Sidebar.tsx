769b310930467eb122a77ce55ba51fb411f12a3c
totegamma <github@gammalab.net>
2026-01-06 01:35:17 +0900

    add dialog

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { ReactNode, useState } from 'react'
+
 import { useTheme } from '../contexts/Theme'
 import { useClient } from '../contexts/Client'
 
@@ -13,7 +15,10 @@ import { ProfileView } from '../views/Profile'
 import { MdPerson } from 'react-icons/md'
 import { MdTerminal } from 'react-icons/md'
 import { MdSettings } from 'react-icons/md'
-import { ReactNode } from 'react'
+import { MdTravelExplore } from 'react-icons/md'
+import { Dialog } from '../ui/Dialog'
+import { TextField } from '../ui/TextField'
+import { Button } from '../ui/Button'
 
 interface Props {
     onPush?: (view: ReactNode) => void
@@ -23,55 +28,85 @@ export const Sidebar = (props: Props) => {
     const theme = useTheme()
     const { client } = useClient()
 
+    const [open, setOpen] = useState(false)
+    const [query, setQuery] = useState('')
+
     return (
-        <div
-            style={{
-                width: '100%',
-                height: '100%',
-                backgroundColor: theme.ui.background,
-                color: theme.ui.text,
-                display: 'flex',
-                padding: '16px',
-                paddingTop: 'env(safe-area-inset-top)',
-                paddingBottom: 'env(safe-area-inset-bottom)',
-                flexDirection: 'column',
-                gap: '16px'
-            }}
-        >
+        <>
             <div
                 style={{
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: theme.ui.background,
+                    color: theme.ui.text,
                     display: 'flex',
+                    padding: '16px',
+                    paddingTop: 'env(safe-area-inset-top)',
+                    paddingBottom: 'env(safe-area-inset-bottom)',
                     flexDirection: 'column',
-                    justifyContent: 'center'
-                }}
-                onClick={() => props.onPush?.(<ProfileView id={client?.ccid || ''} />)}
-            >
-                <Avatar ccid={client?.ccid || ''} src={client?.user?.profile.avatar} />
-                <Text variant="h2">{client?.user?.profile.username || 'Unknown User'}</Text>
-                <Text variant="caption">{client?.server.domain || 'Unknown Server'}</Text>
-            </div>
-            <Divider />
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    gap: '16px',
-                    overflowY: 'auto'
+                    gap: '16px'
                 }}
             >
-                <ListItem
-                    icon={<MdPerson size={24} />}
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center'
+                    }}
                     onClick={() => props.onPush?.(<ProfileView id={client?.ccid || ''} />)}
                 >
-                    プロフィール
-                </ListItem>
-                <ListItem icon={<MdTerminal size={24} />} onClick={() => props.onPush?.(<DevView />)}>
-                    開発者ツール
-                </ListItem>
-                <ListItem icon={<MdSettings size={24} />} onClick={() => props.onPush?.(<SettingsView />)}>
-                    設定
-                </ListItem>
+                    <Avatar ccid={client?.ccid || ''} src={client?.user?.profile.avatar} />
+                    <Text variant="h2">{client?.user?.profile.username || 'Unknown User'}</Text>
+                    <Text variant="caption">{client?.server.domain || 'Unknown Server'}</Text>
+                </div>
+                <Divider />
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '16px',
+                        overflowY: 'auto'
+                    }}
+                >
+                    <ListItem
+                        icon={<MdPerson size={24} />}
+                        onClick={() => props.onPush?.(<ProfileView id={client?.ccid || ''} />)}
+                    >
+                        プロフィール
+                    </ListItem>
+                    <ListItem icon={<MdTravelExplore size={24} />} onClick={() => setOpen(true)}>
+                        照会
+                    </ListItem>
+                    <ListItem icon={<MdTerminal size={24} />} onClick={() => props.onPush?.(<DevView />)}>
+                        開発者ツール
+                    </ListItem>
+                    <ListItem icon={<MdSettings size={24} />} onClick={() => props.onPush?.(<SettingsView />)}>
+                        設定
+                    </ListItem>
+                </div>
             </div>
-        </div>
+            <Dialog
+                open={open}
+                onClose={() => setOpen(false)}
+                style={{
+                    padding: '8px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '8px'
+                }}
+            >
+                <Text variant="h3">照会</Text>
+                <TextField value={query} onChange={(e) => setQuery(e.target.value)} placeholder="cc://" />
+                <Button
+                    onClick={() => {
+                        //props.onPush?.(<ProfileView id={query} />)
+                        //setOpen(false)
+                        //setQuery('')
+                    }}
+                >
+                    照会
+                </Button>
+            </Dialog>
+        </>
     )
 }
---
+++ app/src/ui/Dialog.tsx
@@ -0,0 +1,53 @@
+import { CSSProperties } from 'react'
+import { useTheme } from '../contexts/Theme'
+import { AnimatePresence, motion } from 'motion/react'
+
+interface Props {
+    open: boolean
+    onClose: () => void
+    children: React.ReactNode
+    style?: CSSProperties
+}
+
+export const Dialog = (props: Props) => {
+    const theme = useTheme()
+
+    if (!props.open) {
+        return null
+    }
+
+    return (
+        <AnimatePresence>
+            <motion.div
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+                onClick={props.onClose}
+            >
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '30%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        backgroundColor: theme.content.background,
+                        width: '80vw',
+                        ...props.style
+                    }}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    {props.children}
+                </div>
+            </motion.div>
+        </AnimatePresence>
+    )
+}
