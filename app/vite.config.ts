f40bc44061ab6cb4272d46af7ae119eb44e2a10c
totegamma <github@gammalab.net>
2026-01-16 14:52:56 +0900

    add reactFallbackThrottlePlugin

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import reactFallbackThrottlePlugin from 'vite-plugin-react-fallback-throttle'
 import react from '@vitejs/plugin-react'
 
 // @ts-expect-error process is a nodejs global
@@ -6,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-    plugins: [react()],
+    plugins: [react(), reactFallbackThrottlePlugin()],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
