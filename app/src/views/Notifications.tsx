dfbb18c405425e289be0cb7cce1f326f26b91f67
totegamma <github@gammalab.net>
2026-02-10 23:42:15 +0900

    update notification view

--- app/src/views/Notifications.tsx
+++ app/src/views/Notifications.tsx
@@ -1,10 +1,15 @@
 import { View } from '../ui/View'
 import { Header } from '../ui/Header'
+import { QueryTimeline } from '../components/QueryTimeline'
+import { useClient } from '../contexts/Client'
 
 export const NotificationsView = () => {
+    const { client } = useClient()
+
     return (
         <View>
             <Header>Notifications</Header>
+            <QueryTimeline prefix={`cckv://${client?.ccid}/concrnt.world/main/notify-timeline/`} />
         </View>
     )
 }
