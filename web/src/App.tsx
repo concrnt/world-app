9efe08481adecb84e94110480ba18950abb70279
totegamma <github@gammalab.net>
2026-02-25 03:12:45 +0900

    tweak web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,38 @@
+import { QRCode } from 'react-qrcode-logo'
+import { Text } from '@concrnt/ui'
+// import { type Document } from '@concrnt/client'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    /*
+    const doc: Document<any> = {
+        schema: 'https://concrnt.com/schemas/document/v1',
+
+    }
+    */
+
+    const [sessionID, _] = useState<string>(crypto.randomUUID())
+
+    return (
+        <div>
+            <Text variant="h3">{sessionID}</Text>
+            <QRCode
+                value={sessionID}
+                size={500}
+                ecLevel="L"
+                quietZone={50}
+                qrStyle="fluid"
+                eyeRadius={2}
+                style={{
+                    width: '100%',
+                    height: '100%'
+                }}
+                fgColor={'black'}
+                bgColor={'transparent'}
+            />
+        </div>
+    )
 }
 
 export default App
